40a8ff93d9e7ebb702c76f522bf1a2c5fde0667f
LxfGitHub <1121232426@qq.com>
2019-08-10 11:48:08 +0900

    create project

---
+++ CurrencyConversion/CurrencyConversion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyConversion
+//
+//  Created by luoxiaofeng on 2019/08/10.
+//  Copyright © 2019年 luoxiaofeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
